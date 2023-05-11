28ed45bfb73270b669ca11292c338e7b11b4c6c0
Ethan <ethangroene@gmail.com>
2023-05-11 00:58:37 -0600

    Style login option headers
    If selected, the selected class is applied, and vice versa

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import style from "./Login.module.css";
 
-class Login extends React.Component {}
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = { loginSelected: true };
+  }
+
+  selectLoginMethod = (e) => {
+    e.target.id === "logIn"
+      ? this.setState({ loginSelected: true })
+      : this.setState({ loginSelected: false });
+  };
+
+  render() {
+    return (
+      <div id="homepageContainer">
+        <header>Welcome to codeCommerce!</header>
+        <div id={style.homepageOptions}>
+          <header
+            id="logIn"
+            className={
+              this.state.loginSelected ? style.selected : style.unselected
+            }
+            onClick={this.selectLoginMethod}
+          >
+            Log In
+          </header>
+          <header
+            id="signUp"
+            className={
+              !this.state.loginSelected ? style.selected : style.unselected
+            }
+            onClick={this.selectLoginMethod}
+          >
+            Sign Up
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Login;
