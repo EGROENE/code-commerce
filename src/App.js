b5d7288f223ca1b2f954d2fd938bab67c550d4c1
Ethan <ethangroene@gmail.com>
2023-05-13 22:57:28 -0600

    Init state/props, add expl comments

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import "./App.css";
 import Login from "./Components/Login/Login";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplayedPage: false,
+    };
+  }
+
+  // Method to move to next page:
+
+  // Method to go to previous page:
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>codeCommerce</h1>
-          <Login />
+          <Login isDisplayedPage={this.state.isDisplayedPage} />
         </header>
       </div>
     );
