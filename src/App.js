82f2dd08d4e70ae780e5cf431e9d94893471917d
Ethan <ethangroene@gmail.com>
2023-05-13 21:20:12 -0600

    Convert to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React from "react";
 import logo from "./new-logo.png";
 import "./App.css";
 import Login from "./Components/Login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>codeCommerce</h1>
-        <Login />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>codeCommerce</h1>
+          <Login />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
