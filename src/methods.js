e9c894ef5b65fb52c4fee93a2df1d0114cc9a3c9
Ethan <ethangroene@gmail.com>
2023-05-17 22:19:27 -0600

    Init methods.js w/ round-to-hundredth method

---
+++ src/methods.js
@@ -0,0 +1,8 @@
+export const roundToHundredth = (val) => {
+  const newVal = val.toFixed(3);
+  if (newVal.charAt(newVal.length - 1) === "5") {
+    return Number(Number(newVal + "1").toFixed(2));
+  } else {
+    return Number(Number(newVal).toFixed(2));
+  }
+};
