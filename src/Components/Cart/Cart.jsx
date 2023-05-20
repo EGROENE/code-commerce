edc2c7c620966db0ad57056b623b055e3491456f
Ethan <ethangroene@gmail.com>
2023-05-19 22:21:44 -0600

    Update item state quantity value when changed

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,131 +1,118 @@
 import React from "react";
+import style from "./Cart.module.css";
+import { ITEMS_IN_CART } from "../../Constants/itemsAddedToCart.js";
 import { roundToHundredth } from "../../methods";
 
 class Cart extends React.Component {
-  constructor() {
-    super();
+  // Should addedItems array be here, then mapped through in init of state below?
+  constructor(props) {
+    super(props);
     this.state = {
-      quantityFieldsChangeTally: 0, // May not be needed
-      quantities: {
-        hawaiianRainbowWomensTee: 2,
-        alaskanSkyMensTee: 1,
-      },
+      itemsInCart: ITEMS_IN_CART,
     };
   }
 
-  // Method to update item quantity state values. Called onChange of quantity field:
-  updateItemQuantity = (e, itemName) => {
-    let quantity = Number(e.target.value);
+  componentDidMount = () => {
+    console.log(this.state.itemsInCart);
+    console.log(this.state.itemsInCart[0]);
+    console.log(this.state.itemsInCart[1]);
+    /* const itemsInCartWithQuantity = [...this.state.itemsInCart].map((item) => ({
+      ...item,
+      quantity: 1,
+    }));
+    console.log(itemsInCartWithQuantity);
+    this.setState({ itemsInCart: itemsInCartWithQuantity }); */
+  };
+
+  // Method to update prices onChange of quantity field:
+  // Also set state value of item's quantity to what is in the field:
+  updatePrices = (e, itemNameCamelCase) => {
+    let newQuantity = Number(e.target.value);
+    let selectedItem = this.state.itemsInCart.filter((item) => {
+      return item.itemNameCamelCase === itemNameCamelCase;
+    })[0];
+
+    let selectedItemIndex = this.state.itemsInCart.indexOf(selectedItem);
+    console.log(this.state.itemsInCart[selectedItemIndex]);
+
     this.setState((prevState) => ({
-      quantities: {
-        ...prevState.quantities,
-        [itemName]: quantity,
-      },
+      ...prevState,
+      itemsInCart: prevState.itemsInCart.map((item) =>
+        this.state.itemsInCart.indexOf(item) === selectedItemIndex
+          ? { ...item, quantity: newQuantity }
+          : item
+      ),
     }));
   };
 
+  // Method to update prices.
+  // Update item quantity state values.
+  // Called onChange of quantity field:
+  updateItemQuantityPriceCartTotals = (e, item, itemName) => {};
+
+  // Method to update cart totals:
+  // Also call on change of quantity field.
+  updateTotals = () => {};
+
+  // Check input in promo code box to see if it matches an available promo. Update prices accordingly.
+  // Should set discount value in state and change cartTotal if match.
+  // Call onClick of apply button.
+  checkPromoCode = (e) => {};
+
   render() {
     const { isCartHidden } = this.props;
-
-    const itemsAddedToCart = [
-      {
-        itemName: "Hawaiian Rainbow Women's Tee",
-        itemNameCamelCase: "hawaiianRainbowWomensTee",
-        itemImage:
-          "https://d1ozqqh7vh3ykm.cloudfront.net/2022/38/95478030/thumb_97286a023d362e1.png",
-        gender: "Women",
-        color: "Leaf Green",
-        size: "S",
-        unitPrice: 24.99,
-        totalItemPrice: 24.99,
-        itemQuantity: this.state.quantities.hawaiianRainbowWomensTee,
-      },
-      /* {
-        itemName: "Desert Sunset Women's Eco Tee",
-        itemImage:
-          "https://d1ozqqh7vh3ykm.cloudfront.net/2022/29/91393993/thumb_3ddf445c967ab8d.png",
-        gender: "Women",
-        color: "Navy Blue",
-        size: "M",
-        unitPrice: 29.99,
-        totalItemPrice: 29.99,
-      }, */
-      /* {
-        itemName: "Jamaican Logo Women's Tee",
-        itemImage:
-          "https://d1ozqqh7vh3ykm.cloudfront.net/2021/50/76865028/thumb_bf3c96108c3aebe.png",
-        gender: "Women",
-        color: "White",
-        size: "M",
-        unitPrice: 24.99,
-        totalItemPrice: 24.99,
-      }, */
-      {
-        itemName: "Alaskan Sky Men's Tee",
-        itemNameCamelCase: "alaskanSkyMensTee",
-        itemImage:
-          "https://d1ozqqh7vh3ykm.cloudfront.net/2021/14/56822314/thumb_afe8edd43024014.png",
-        gender: "Men",
-        color: "All-Over",
-        size: "M",
-        unitPrice: 29.99,
-        totalItemPrice: 29.99,
-        itemQuantity: this.state.quantities.alaskanSkyMensTee,
-      },
-      /* {
-        itemName: "Hibiscus Men's Tee",
-        itemNameCamelCase: "hibiscusMensTee",
-        itemImage:
-          "https://d1ozqqh7vh3ykm.cloudfront.net/2021/08/52293478/thumb_8ba849f7b657f8f.png",
-        gender: "Men",
-        color: "Mint Green",
-        size: "L",
-        unitPrice: 19.99,
-        totalItemPrice: 19.99,
-      }, */
-    ];
 
     return (
       <div hidden={isCartHidden}>
         <h1>Cart</h1>
         <div id="cartPageContainer">
-          <div id="itemsInCart">
-            {itemsAddedToCart.map(
-              (item) =>
-                this.state.quantities[`${item.itemNameCamelCase}`] !== 0 && (
-                  <div id={item.itemName} className="itemInCart">
-                    <img alt="" src={item.itemImage} />
-                    <div id="itemInfo">
-                      <p>{item.gender}</p>
-                      <p>{item.itemName}</p>
-                      <p>Color: {item.color}</p>
-                      <p>Size: {item.size}</p>
-                    </div>
-                    <p>{item.unitPrice}</p>
-                    <input
-                      value={this.state.quantities[`${item.itemNameCamelCase}`]}
-                      type="number"
-                      min={1}
-                      step={1}
-                      onChange={(e) =>
-                        this.updateItemQuantity(e, item.itemNameCamelCase)
-                      }
-                    />
-                    <p>
-                      {"$" +
+          <div id={style.itemsInCart}>
+            {this.state.itemsInCart.map((item) => (
+              <div id={item.itemNameCamelCase} className={style.itemInCart}>
+                <img alt="" src={item.itemImage} />
+                <div className={style.itemInfo}>
+                  <p>{item.gender}</p>
+                  <p>{item.itemName}</p>
+                  <p>Color: {item.color}</p>
+                  <p>Size: {item.size}</p>
+                </div>
+                <p>{"$" + item.unitPrice}</p>
+                <input
+                  value={item.quantity}
+                  type="number"
+                  min={1}
+                  step={1}
+                  onChange={(e) => {
+                    this.updatePrices(e, item.itemNameCamelCase);
+                  }}
+                />
+                <p>
+                  {/* Set total item price here w/ this.state.totalItemPrices[itemNameCamelCase], which is updated in updatetotalItemPrices method */}
+                  {/* {"$" +
                         roundToHundredth(
                           this.state.quantities[`${item.itemNameCamelCase}`] *
                             item.unitPrice
                         ).toLocaleString(undefined, {
                           minimumFractionDigits: 2,
                           maximumFractionDigits: 2,
-                        })}
-                    </p>
-                  </div>
-                )
-            )}
+                        })} */}
+                  {}
+                </p>
+              </div>
+            ))}
           </div>
-          <div id="cartSummary"></div>
+          <div id="cartSummary">
+            <header>Cart Summary</header>
+            <label htmlFor="">
+              <p>Do you have a promo code?</p>
+              <input type="text" />
+              <button id={style.applyPromo}>Apply</button>
+            </label>
+            <p>Cart Subtotal: {}</p>
+            <p>Shipping & Handling: -</p>
+            <p>Discount: {}</p>
+            <p>Cart Total: {}</p>
+          </div>
         </div>
       </div>
     );
