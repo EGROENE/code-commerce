8f0fd1efd08560d29c15a1d66770b90bb25d01b6
Ethan <ethangroene@gmail.com>
2023-05-23 23:03:54 -0600

    Modify toNextPage(), pass as prop to Cart.jsx
    Modification makes it possible for this method to accept params. Each component then passes e, its own hidden state key (EX: isCartHidden) & that of the next page (component). Current-pg hidden state changes to true, the next to false.

--- src/App.js
+++ src/App.js
@@ -21,27 +21,19 @@ class App extends React.Component {
   // Method to move to next page:
   // Pass to Login.jsx for onClick of Sign in or Create Account btn
   // Must change this.state.isLoginHidden to true
-  toNextPage = (e) => {
+  toNextPage = (e, selectedPageHidden, nextPageHidden) => {
     e.preventDefault();
 
     // Only move on if there are no errors in error object! ---- if each error === ""
     // May need to pass errors object from each comp to App.js or init this in App.js then export as prop to components
     // Or, find a way to only call this in each comp if its own error state object contains no errors (if all Object.values of this.state.errors === "", then call this onClick. Requires conditional rendering in this onClick)
 
-    /* let pageDisplayed = Object.entries(this.state.pageDisplayOptions).find(
-      (pair) => pair[1] === false
-    );
-    console.log(Object.entries(this.state.pageDisplayOptions));
-    console.log(pageDisplayed);
-    console.log(pageDisplayed[0]);
-    let currentDisplayedPage = this.state.pageDisplayOptions[pageDisplayed[0]];
-    console.log(currentDisplayedPage); */
-    if (!this.state.pageDisplayOptions.isLoginHidden) {
+    if (!this.state.pageDisplayOptions[selectedPageHidden]) {
       this.setState((prevState) => ({
         pageDisplayOptions: {
           ...prevState.pageDisplayOptions,
-          isLoginHidden: true,
-          isCartHidden: false,
+          [selectedPageHidden]: true,
+          [nextPageHidden]: false,
         },
       }));
     }
@@ -59,7 +51,10 @@ class App extends React.Component {
             isLoginHidden={this.state.pageDisplayOptions.isLoginHidden}
             toNextPage={this.toNextPage}
           />
-          <Cart />
+          <Cart
+            isCartHidden={this.state.pageDisplayOptions.isCartHidden}
+            toNextPage={this.toNextPage}
+          />
         </header>
       </div>
     );
