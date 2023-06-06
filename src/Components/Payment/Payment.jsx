9b08f234da89a1e59af7bee4deb1126f060c44da
Ethan <ethangroene@gmail.com>
2023-06-06 00:55:52 -0600

    Use ProgressBar comp to pop the same, based on appropriate values

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -1,20 +1,12 @@
 import React from "react";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 class Payment extends React.Component {
   render() {
-    const { isPaymentHidden } = this.props;
-    console.log(isPaymentHidden);
+    const { isPaymentHidden, completedPages } = this.props;
     return (
       <div hidden={isPaymentHidden}>
-        <div className="progressBar">
-          <i className="fas fa-shopping-cart completed"></i>
-          <div className="progressBarConnector completed"></div>
-          <i className="fas fa-shipping-fast completed"></i>
-          <div className="progressBarConnector completed"></div>
-          <i className="fas fa-money-check-alt"></i>
-          <div className="progressBarConnector"></div>
-          <i className="fas fa-check"></i>
-        </div>
+        <ProgressBar completedPages={completedPages} />
         <header className="pageHeader">Payment</header>
       </div>
     );
