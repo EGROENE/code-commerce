d891683865719e7677a47f3bb20a33a27b858e33
Ethan <ethangroene@gmail.com>
2023-06-09 23:02:47 -0600

    Put exp date, CVV on same line; add back/pay btn
    Also, add header to section of order summary that details delivery info

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -89,56 +89,74 @@ class Payment extends React.Component {
                   placeholder="Enter card number"
                 />
               </label>
-              <label htmlFor="">
-                <header>Expiry Date: </header>
-                <select name="" id="">
-                  <option disabled selected>
-                    Month
-                  </option>
-                  <option value="01">01</option>
-                  <option value="02">02</option>
-                  <option value="03">03</option>
-                  <option value="04">04</option>
-                  <option value="05">05</option>
-                  <option value="06">06</option>
-                  <option value="07">07</option>
-                  <option value="08">08</option>
-                  <option value="09">09</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                </select>
-                <select name="" id="">
-                  <option disabled selected>
-                    Year
-                  </option>
-                  <option value="2023">2023</option>
-                  <option value="2024">2024</option>
-                  <option value="2025">2025</option>
-                  <option value="2026">2026</option>
-                  <option value="2027">2027</option>
-                  <option value="2028">2028</option>
-                  <option value="2029">2029</option>
-                  <option value="2030">2030</option>
-                  <option value="2031">2031</option>
-                  <option value="2032">2032</option>
-                  <option value="2033">2033</option>
-                  <option value="2034">2034</option>
-                  <option value="2035">2035</option>
-                  <option value="2036">2036</option>
-                  <option value="2037">2037</option>
-                  <option value="2038">2038</option>
-                </select>
-              </label>
-              <label htmlFor="">
-                <header>CVV</header>
-                <input
-                  type="text"
-                  required
-                  inputMode="numeric"
-                  placeholder="Enter 3-digit CVV code"
-                />
-              </label>
+              <div id={style.expiryAndCVV}>
+                <label htmlFor="">
+                  <header>Expiry Date: </header>
+                  <select name="" id="">
+                    <option disabled selected>
+                      Month
+                    </option>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                    <option value="03">03</option>
+                    <option value="04">04</option>
+                    <option value="05">05</option>
+                    <option value="06">06</option>
+                    <option value="07">07</option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+                  <select name="" id="">
+                    <option disabled selected>
+                      Year
+                    </option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                    <option value="2030">2030</option>
+                    <option value="2031">2031</option>
+                    <option value="2032">2032</option>
+                    <option value="2033">2033</option>
+                    <option value="2034">2034</option>
+                    <option value="2035">2035</option>
+                    <option value="2036">2036</option>
+                    <option value="2037">2037</option>
+                    <option value="2038">2038</option>
+                  </select>
+                </label>
+                <label htmlFor="">
+                  <header>CVV:</header>
+                  <input
+                    type="text"
+                    required
+                    inputMode="numeric"
+                    placeholder="Enter 3-digit CVV code"
+                  />
+                </label>
+              </div>
+              <div className="checkoutBackNextBtnContainer">
+                <button
+                  onClick={(e) => {
+                    toPreviousPage(e, "isShippingHidden", "isPaymentHidden");
+                  }}
+                >
+                  Back to Shipping
+                </button>
+                <button>
+                  Pay $
+                  {cartTotal.toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </button>
+              </div>
             </form>
             <div className="cartSummary">
               <div className="cartSummaryHeaders">
@@ -188,6 +206,7 @@ class Payment extends React.Component {
                 ))}
               </div>
               <div className="cartSummaryItem">
+                <header>Delivery Information</header>
                 <p>
                   {shipmentDetails.title !== "" && shipmentDetails.title}{" "}
                   {shipmentDetails.name}
