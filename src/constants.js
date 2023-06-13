0a73f7e3e32b8508c10a183d312ccbf59479e6f2
Ethan <ethangroene@gmail.com>
2023-06-13 00:19:07 -0600

    Take constants.js out of folder, del folder
    Only change to the document is adding a series of RegEx patterns for the accepted payment cards.

---
+++ src/constants.js
@@ -0,0 +1,29 @@
+export const ITEMS_IN_CART = [
+  {
+    itemName: "The Ultimate JS RegEx Pack",
+    itemNameCamelCase: "theUltimateJSRegExPack",
+    itemImage:
+      "https://d1ozqqh7vh3ykm.cloudfront.net/2023/21/113012875/thumb_96496a1ad239704.png",
+    category: "Web Development",
+    language: "JavaScript",
+    unitPrice: 250.0,
+    quantity: 1,
+  },
+  {
+    itemName: "Top 50 CSS Animations Snippets",
+    itemNameCamelCase: "top50CSSAnimationsSnippets",
+    itemImage:
+      "https://d1ozqqh7vh3ykm.cloudfront.net/2023/21/113013054/thumb_a9244058c5c1cb3.png",
+    category: "Web Development",
+    language: "Cascading Style Sheets",
+    unitPrice: 75.0,
+    quantity: 2,
+  },
+];
+
+export const cardRegexPatterns = {
+  MASTERCARD: /^5[1-5][0-9]{1,}|^2[2-7][0-9]{1,}$/,
+  VISA: /^4[0-9]{2,}$/,
+  AMERICAN_EXPRESS: /^3[47][0-9]{5,}$/,
+  DISCOVER: /^6(?:011|5[0-9]{2})[0-9]{3,}$/,
+};
