b9266759b3139d7a3aa1398a3b1a5c5f9c3e7590
Ethan <ethangroene@gmail.com>
2023-06-13 22:29:42 -0600

    Import deliveryTime, display in delivery info section

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -137,6 +137,7 @@ class Payment extends React.Component {
       toPreviousPage,
       shipmentDetails,
       shippingAndHandling,
+      deliveryTime,
     } = this.props;
 
     // Calculate totals based on current state values of unit prices & quantity:
@@ -334,6 +335,7 @@ class Payment extends React.Component {
                     <p>E-mail: {accountEmailAddress}</p>
                   </div>
                 </div>
+                <p>- Delivery within {deliveryTime} of placing order -</p>
               </div>
             </form>
             <div className="cartSummary">
