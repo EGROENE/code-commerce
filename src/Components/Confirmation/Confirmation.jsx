33cecc0b9f4ffb5f2362678633214d0f62efe382
Ethan <ethangroene@gmail.com>
2023-08-07 18:41:07 -0400

    Make it possible to exit order details onclick outside of modal

--- src/Components/Confirmation/Confirmation.jsx
+++ src/Components/Confirmation/Confirmation.jsx
@@ -77,100 +77,102 @@ class Confirmation extends React.Component {
         </div>
         <div
           id={style.modalContainer}
+          onClick={
+            this.state.isOrderSummaryDisplayed && this.showHideOrderDetails
+          }
           style={
             this.state.isOrderSummaryDisplayed
               ? { display: "flex", position: "fixed" }
               : { display: "none", position: "unset" }
           }
+        ></div>
+        <div
+          style={
+            this.state.isOrderSummaryDisplayed
+              ? { display: "block" }
+              : { display: "none" }
+          }
+          id={style.orderSummaryContainer}
         >
-          <div
-            style={
-              this.state.isOrderSummaryDisplayed
-                ? { display: "block" }
-                : { display: "none" }
-            }
-            id={style.orderSummaryContainer}
-          >
-            <i
-              title="Close Order Summary"
-              onClick={this.showHideOrderDetails}
-              id={style.closeModalBtn}
-              className="fas fa-times"
-            ></i>
-            <header id={style.modalHeader}>Order Details</header>
-            <div id={style.orderItemsAndTotals}>
-              <div
-                id={style.itemsInCartContainer}
-                style={
-                  numberOfItemsInCart > 2
-                    ? { overflowY: "scroll" }
-                    : { overflowY: "unset" }
-                }
-              >
-                {itemsInCart.map(
-                  (item) =>
-                    item.quantity > 0 && (
-                      <div
-                        key={item.itemNameCamelCase}
-                        className={style.itemInOrderSummary}
-                      >
-                        <img src={item.itemImage} alt="Item" />
-                        <div className={style.itemDetailsShippingPage}>
-                          <p>{item.itemName}</p>
-                          <p>
-                            Category: <span>{item.category}</span>
-                          </p>
-                          <p>
-                            Language: <span>{item.language}</span>
-                          </p>
-                          <p>
-                            Quantity: <span>{item.quantity}</span>
-                          </p>
-                          <p className="itemInfoHeader">
-                            Item Total:
-                            <span>
-                              {" $" +
-                                roundToHundredth(
-                                  item.quantity * item.unitPrice
-                                ).toLocaleString(undefined, {
-                                  minimumFractionDigits: 2,
-                                  maximumFractionDigits: 2,
-                                })}
-                            </span>
-                          </p>
-                        </div>
+          <i
+            title="Close Order Summary"
+            onClick={this.showHideOrderDetails}
+            id={style.closeModalBtn}
+            className="fas fa-times"
+          ></i>
+          <header id={style.modalHeader}>Order Details</header>
+          <div id={style.orderItemsAndTotals}>
+            <div
+              id={style.itemsInCartContainer}
+              style={
+                numberOfItemsInCart > 2
+                  ? { overflowY: "scroll" }
+                  : { overflowY: "unset" }
+              }
+            >
+              {itemsInCart.map(
+                (item) =>
+                  item.quantity > 0 && (
+                    <div
+                      key={item.itemNameCamelCase}
+                      className={style.itemInOrderSummary}
+                    >
+                      <img src={item.itemImage} alt="Item" />
+                      <div className={style.itemDetailsShippingPage}>
+                        <p>{item.itemName}</p>
+                        <p>
+                          Category: <span>{item.category}</span>
+                        </p>
+                        <p>
+                          Language: <span>{item.language}</span>
+                        </p>
+                        <p>
+                          Quantity: <span>{item.quantity}</span>
+                        </p>
+                        <p className="itemInfoHeader">
+                          Item Total:
+                          <span>
+                            {" $" +
+                              roundToHundredth(
+                                item.quantity * item.unitPrice
+                              ).toLocaleString(undefined, {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                              })}
+                          </span>
+                        </p>
                       </div>
-                    )
-                )}
+                    </div>
+                  )
+              )}
+            </div>
+            <div id={style.detailsContainer}>
+              <div id={style.orderTotalsContainer}>
+                <header>Cart Totals:</header>
+                {summaryTotals.map((total) => (
+                  <p key={total.label}>
+                    {total.label} $
+                    {total.value.toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                  </p>
+                ))}
               </div>
-              <div id={style.detailsContainer}>
-                <div id={style.orderTotalsContainer}>
-                  <header>Cart Totals:</header>
-                  {summaryTotals.map((total) => (
-                    <p key={total.label}>
-                      {total.label} $
-                      {total.value.toLocaleString(undefined, {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      })}
-                    </p>
-                  ))}
-                </div>
-                <div id={style.orderShipmentDetails}>
-                  <header>Delivering To:</header>
-                  <p>
-                    {shipmentDetails.title !== "" && shipmentDetails.title}{" "}
-                    {shipmentDetails.name}
-                  </p>
-                  <p>Card ending in {lastDigitsOfCardNumber}</p>
-                  <p>{shipmentDetails.streetAddress}</p>
-                  <p>
-                    {shipmentDetails.city}, {shipmentDetails.stateOrTerritory}{" "}
-                    {shipmentDetails.postalCode}
-                  </p>
-                  <p>Phone: {shipmentDetails.phoneNumberMask}</p>
-                  <p>E-mail: {accountEmailAddress}</p>
-                </div>
+              <div id={style.orderShipmentDetails}>
+                <header>Delivering To:</header>
+                <p>
+                  {shipmentDetails.title !== "" && shipmentDetails.title}{" "}
+                  {shipmentDetails.name}
+                </p>
+                <p>Card ending in {lastDigitsOfCardNumber}</p>
+                <p>{shipmentDetails.streetAddress}</p>
+                <p>
+                  {shipmentDetails.city}, {shipmentDetails.stateOrTerritory}{" "}
+                  {shipmentDetails.postalCode}
+                </p>
+                <p>Phone: {shipmentDetails.phoneNumberMask}</p>
+                <p>E-mail: {accountEmailAddress}</p>
               </div>
             </div>
           </div>
