65be4859a531f0dd0adfccd42c9cc467edcc67e7
Ethan Groene <ethangroene@gmail.com>
2023-09-27 23:52:40 -0600

    Change email validation method used

--- src/validations.js
+++ src/validations.js
@@ -1,16 +1,16 @@
 import { cardRegexPatterns } from "./constants";
 
-/* export const isEmailValid = (value) => {
+export const emailIsValid = (value) => {
   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     value.trim()
   );
-}; */
+};
 
-export function isEmailValid(value) {
+/* export function emailIsValid(value) {
   // eslint-disable-next-line no-useless-escape
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return !!value.match(regex);
-}
+} */
 
 export const passwordIsValid = (value) => {
   return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,20}$/.test(
