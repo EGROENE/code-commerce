5d19cfd3085c0bc4ac002bf3f7386dc131a53133
Ethan Groene <ethangroene@gmail.com>
2023-10-02 12:51:02 -0600

    Create function version of ProgressBar comp

---
+++ src/Components/ProgressBar/FunctionalProgressBar.jsx
@@ -0,0 +1,59 @@
+const FunctionalProgressBar = ({ arePagesComplete }) => {
+  const progressBarElements = [
+    {
+      id: "Cart",
+      class: "fas fa-shopping-cart",
+      isCompleted: arePagesComplete.isCartComplete,
+    },
+    {
+      id: "Shipping",
+      class: "fas fa-shipping-fast",
+      isCompleted: arePagesComplete.isShippingComplete,
+    },
+    {
+      id: "Payment",
+      class: "fas fa-money-check-alt",
+      isCompleted: arePagesComplete.isPaymentComplete,
+    },
+    {
+      id: "Confirmation",
+      class: "fas fa-check",
+      isCompleted: arePagesComplete.isPaymentComplete,
+    },
+  ];
+
+  const isElementLast = (element) => {
+    return progressBarElements.indexOf(element) ===
+      progressBarElements.length - 1
+      ? true
+      : false;
+  };
+
+  return (
+    <div className="progressBar">
+      {progressBarElements.map((item) => (
+        <div key={item.id} className="progressBarElement">
+          <div>
+            <i
+              className={
+                item.isCompleted ? "fas fa-check completed" : item.class
+              }
+            ></i>
+            <p>{item.id}</p>
+          </div>
+          {!isElementLast(item) && (
+            <div
+              className={
+                item.isCompleted
+                  ? "progressBarConnector completed"
+                  : "progressBarConnector"
+              }
+            ></div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FunctionalProgressBar;
